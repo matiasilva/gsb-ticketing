656496ff176523ae963ef71cba4cc87cb28ab82f
k12ish <k4krish@gmail.com>
2023-03-16 12:03:57 +0000

    modify plan_b script for name fix csv override (AAAAAAAAAAAAARRRRRRRRRRGH)

--- scripts/plan_b/csv_to_plan_b.py
+++ scripts/plan_b/csv_to_plan_b.py
@@ -2,6 +2,7 @@ import argparse
 import csv
 from datetime import datetime
 from pathlib import Path
+from typing import Dict, Iterator
 
 from pylatex import (
     Document,
@@ -17,36 +18,43 @@ from pylatex.utils import NoEscape, bold
 
 parser = argparse.ArgumentParser(description="generate ticketing plan b")
 parser.add_argument("csv_file", type=Path, help="csv file path")
+parser.add_argument(
+    "-o",
+    "--override",
+    metavar="NEW_CSV_FILE",
+    type=Path,
+    help="generate SVGs from entries in newer file",
+)
 args = parser.parse_args()
 
 
 OUT_DIR = Path("pdfs")
 OUT_DIR.mkdir(exist_ok=True)
 
-STYLE = PageStyle("firstpage")
-
-with STYLE.create(Head("R")) as right_header:
-    with right_header.create(
-        MiniPage(width=NoEscape(r"0.49\textwidth"), pos="c", align="r")
-    ) as r_title:
-        r_title.append(LargeText(bold("Plan B")))
-        r_title.append(LineBreak())
-        r_title.append(
-            MediumText(
-                bold(datetime.now().replace(second=0, microsecond=0).isoformat())
-            )
-        )
-
-with STYLE.create(Head("L")) as left_header:
-    with left_header.create(
-        MiniPage(width=NoEscape(r"0.49\textwidth"), pos="c", align="l")
-    ) as l_title:
-        l_title.append(
-            LargeText(bold(NoEscape(r"Page \thepage\ of \pageref{LastPage}")))
-        )
-
 
 def make_doc(data: list):
+    style = PageStyle("firstpage")
+
+    with style.create(Head("R")) as right_header:
+        with right_header.create(
+            MiniPage(width=NoEscape(r"0.49\textwidth"), pos="c", align="r")
+        ) as r_title:
+            r_title.append(LargeText(bold("Plan B")))
+            r_title.append(LineBreak())
+            r_title.append(
+                MediumText(
+                    bold(datetime.now().replace(second=0, microsecond=0).isoformat())
+                )
+            )
+
+    with style.create(Head("L")) as left_header:
+        with left_header.create(
+            MiniPage(width=NoEscape(r"0.49\textwidth"), pos="c", align="l")
+        ) as l_title:
+            l_title.append(
+                LargeText(bold(NoEscape(r"Page \thepage\ of \pageref{LastPage}")))
+            )
+
     doc = Document(
         geometry_options={
             "head": "40pt",
@@ -56,7 +64,7 @@ def make_doc(data: list):
         }
     )
     doc.change_document_style("firstpage")
-    doc.preamble.append(STYLE)
+    doc.preamble.append(style)
     doc.preamble.append(NoEscape(r"\definecolor{highlight}{gray}{0.88}"))
 
     doc.packages.append(Package('supertabular'))
@@ -82,13 +90,25 @@ def make_doc(data: list):
     return doc
 
 
-with open(args.csv_file, newline="") as csvfile:
-    queue_jump, standard = [], []
-    for row in csv.DictReader(csvfile):
-        if row["kind"].startswith("QJ"):
-            queue_jump.append(row)
-        else:
-            standard.append(row)
+def csv_rows() -> Dict[str, Dict[str, str]]:
+    with open(args.csv_file, newline="") as csvfile:
+        tickets = {row["uuid"]: row for row in csv.DictReader(csvfile)}
+    if args.override:
+        with open(args.override, newline="") as csvfile:
+            tickets.update({row["uuid"]: row for row in csv.DictReader(csvfile)})
+    return tickets
+
+
+QUEUE_JUMP_ROWS, STANDARD_ROWS = [], []
+for row in csv_rows().values():
+    if row["kind"].startswith("QJ"):
+        QUEUE_JUMP_ROWS.append(row)
+    else:
+        STANDARD_ROWS.append(row)
+
+
+print(len(QUEUE_JUMP_ROWS), "number of queue jump tickets")
+print(len(STANDARD_ROWS), "number of standard tickets")
 
 
 def process(tickets: list):
@@ -102,14 +122,14 @@ def process(tickets: list):
         ticket["color"] = "highlight" if toggle else None
 
 
-process(queue_jump)
-process(standard)
+process(QUEUE_JUMP_ROWS)
+process(STANDARD_ROWS)
 
-doc = make_doc(queue_jump)
+doc = make_doc(QUEUE_JUMP_ROWS)
 doc.generate_pdf(
     OUT_DIR / "queue_jump", clean=False, clean_tex=False, compiler="pdflatex"
 )
-doc = make_doc(standard)
+doc = make_doc(STANDARD_ROWS)
 doc.generate_pdf(
     OUT_DIR / "standard", clean=False, clean_tex=False, compiler="pdflatex"
 )
